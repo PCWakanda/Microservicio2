fb07cb50df96a94063be49d1ab5330b6b4e6996e
jmartter <150058251+jmartter@users.noreply.github.com>
2024-11-26 11:37:53 +0100

    Initial commit

---
+++ src/test/java/org/example/microservicio2/Microservicio2ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.microservicio2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Microservicio2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
